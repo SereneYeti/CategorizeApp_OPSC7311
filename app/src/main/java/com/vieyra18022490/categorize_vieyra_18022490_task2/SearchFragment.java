bbf688de92ddcb2ddeb8b06e08a673a846160072
James D Vieyra <james@vieyra.co.za>
2021-07-04 15:44:04 +0200

    Added basic realtime database storage testing wtih hello world message

--- app/src/main/java/com/vieyra18022490/categorize_vieyra_18022490_task2/SearchFragment.java
+++ app/src/main/java/com/vieyra18022490/categorize_vieyra_18022490_task2/SearchFragment.java
@@ -1,26 +1,33 @@
 package com.vieyra18022490.categorize_vieyra_18022490_task2;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class SearchFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private static final String TAG = "SearchFragment";
 
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
     View v;
     private TextView tv_RecieveText3;
+    Button btnPublish_;
 
     public SearchFragment() {
         // Required empty public constructor
@@ -62,6 +69,19 @@ public class SearchFragment extends Fragment {
         {
             tv_RecieveText3.setText(Singleton.getInstance().getTestVar2());
         }
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://categorize-app-poe-prototype-default-rtdb.europe-west1.firebasedatabase.app/");
+        btnPublish_ = v.findViewById(R.id.btnPublish);
+
+        Log.i(TAG, "HELLO! ");
+        btnPublish_.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                DatabaseReference myRef = database.getReference("message");
+                myRef.setValue("Goodbye, Cruel World!");
+                Log.i(TAG, "onClick: ");
+            }
+        });
 
         return v;
     }
--- app/src/main/java/com/vieyra18022490/categorize_vieyra_18022490_task2/SecondFragment.java
+++ app/src/main/java/com/vieyra18022490/categorize_vieyra_18022490_task2/SecondFragment.java
@@ -1,23 +1,32 @@
 package com.vieyra18022490.categorize_vieyra_18022490_task2;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class SecondFragment extends Fragment {
+    private static final String TAG = "SecondFragment";
+    View v;
+
 
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
+        v = inflater.inflate(R.layout.fragment_second, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false);
+        return v;
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
@@ -30,5 +39,7 @@ public class SecondFragment extends Fragment {
                         .navigate(R.id.action_SecondFragment_to_FirstFragment);
             }
         });
+
+
     }
 }
